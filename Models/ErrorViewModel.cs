0ae86070337afef180f70047d15fdf0e6f9d53b1
SohamDas2003 <sohamdas704@gmail.com>
2025-03-23 18:34:23 +0530

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace AIMSR.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
