ef67908cfaeea7f886108a430532664390dc0875
SohamDas2003 <sohamdas704@gmail.com>
2025-03-24 23:03:17 +0530

    commit:2

---
+++ Models/ApplicationUser.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace AIMSR.Areas.Identity.Data
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string FullName { get; set; }
+        public string ProfileImage { get; set; } // Path to profile image
+        public string Department { get; set; } // Relevant for teachers
+        public string Course { get; set; } // Relevant for students
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,27 +1,51 @@
+using Aimsr.Data;
+using Aimsr.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using AIMSR.Areas.Identity.Data;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
+// Add database connection for both DbContexts
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddDbContext<AIMSRIdentityDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+// Add Identity with AIMSRIdentityDbContext
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<AIMSRIdentityDbContext>()
+    .AddDefaultTokenProviders();
+
+// Configure authentication
+builder.Services.ConfigureApplicationCookie(options =>
+{
+    options.LoginPath = "/Account/Login";
+    options.LogoutPath = "/Account/Logout";
+    options.AccessDeniedPath = "/Account/AccessDenied";
+});
+
 builder.Services.AddControllersWithViews();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
+// Migrate Database
+using (var scope = app.Services.CreateScope())
 {
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
+    var services = scope.ServiceProvider;
+
+    var context = services.GetRequiredService<ApplicationDbContext>();
+    context.Database.Migrate();
+
+    var identityContext = services.GetRequiredService<AIMSRIdentityDbContext>();
+    identityContext.Database.Migrate();
 }
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
+app.UseAuthentication();
 app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 app.Run();
